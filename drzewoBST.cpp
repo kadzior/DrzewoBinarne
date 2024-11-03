e75f072a0104f84b721105a979aba2f4e0890468
Kacper Kosal <kacper.kosal@op.pl>
2024-11-03 16:36:34 +0100

    Dodanie metody inorder

--- drzewoBST.cpp
+++ drzewoBST.cpp
@@ -6,7 +6,26 @@ class BST {
 private:
     Node* root;
 
+    void inOrder(Node* node) {
+        if (node != nullptr) {
+            inOrder(node->left);
+            cout << node->data << " ";
+            inOrder(node->right);
+        }
+    }
+
 public:
     BST() : root(nullptr) {}
+
+    void addElement(int value) {
+        root = addElement(root, value);
+    }
+
+    void displayTree(const string& order) {
+        if (order == "inorder") {
+            inOrder(root);
+        } 
+        cout << endl;
+    }
 
 };
