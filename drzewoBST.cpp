72cb592debc36c1f2fa1fa8732d0e4119dcc8dea
Kacper Kosal <kacper.kosal@op.pl>
2024-11-03 16:41:30 +0100

    Dodanie metody postorder

--- drzewoBST.cpp
+++ drzewoBST.cpp
@@ -14,6 +14,22 @@ private:
         }
     }
 
+    void preOrder(Node* node) {
+        if (node != nullptr) {
+            cout << node->data << " ";
+            preOrder(node->left);
+            preOrder(node->right);
+        }
+    }
+
+    void postOrder(Node* node) {
+        if (node != nullptr) {
+            postOrder(node->left);
+            postOrder(node->right);
+            cout << node->data << " ";
+        }
+    }
+
 public:
     BST() : root(nullptr) {}
 
@@ -24,7 +40,13 @@ public:
     void displayTree(const string& order) {
         if (order == "inorder") {
             inOrder(root);
-        } 
+        } else if (order == "preorder") {
+            preOrder(root);
+        } else if (order == "postorder") {
+            postOrder(root);
+        } else {
+            cout << "Invalid order" << endl;
+        }
         cout << endl;
     }
 
